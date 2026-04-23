5ad84baa474fad6476b78630590505bb4735b526
Petr Komin <petr.komin@pkis.cz>
2026-04-23 07:05:32 +0200

    Change default server port from 3000 to 3011

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const rateLimit = require('express-rate-limit');
 const { Zubok } = require('./zubok');
 
 const app  = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3011;
 
 // ── Security headers ───────────────────────────────────────────────
 app.use(helmet({
